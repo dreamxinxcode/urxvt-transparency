6b292ab9a1bcef9e373e0c1c566dae8d5d7b87b7
dreamxinxcode <42306596+dreamxinxcode@users.noreply.github.com>
2018-12-27 23:51:35 -0800

    Update transparency.sh

--- transparency.sh
+++ transparency.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if grep -Fxq "URxvt*transparent: false" /home/<USER>/.Xresources
+if grep -Fxq "URxvt*transparent: false" /home/USER/.Xresources
 then
-	sed -i 's/URxvt\*transparent: false/URxvt\*transparent: true/g' /home/<USER>/.Xresources
+	sed -i 's/URxvt\*transparent: false/URxvt\*transparent: true/g' /home/USER/.Xresources
 	xrdb ~/.Xresources
-else grep -Fxq "URxvt*transparent: true" /home/<USER>/.Xresources
-	sed -i 's/URxvt\*transparent: true/URxvt\*transparent: false/g' /home/<USER>/.Xresources
+else grep -Fxq "URxvt*transparent: true" /home/USER/.Xresources
+	sed -i 's/URxvt\*transparent: true/URxvt\*transparent: false/g' /home/USER/.Xresources
 	xrdb ~/.Xresources
 fi
