02f38b24529e338f2d0a6ccb79a8b16361b9f320
dreamxinxcode <42306596+dreamxinxcode@users.noreply.github.com>
2018-12-27 16:51:20 -0800

    Update transparency.sh

--- transparency.sh
+++ transparency.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-if grep -Fxq "URxvt*transparent: false" /home/gentoo/.Xresources
+if grep -Fxq "URxvt*transparent: false" /home/<USER>/.Xresources
 then
-	sed -i 's/URxvt\*transparent: false/URxvt\*transparent: true/g' /home/gentoo/.Xresources
+	sed -i 's/URxvt\*transparent: false/URxvt\*transparent: true/g' /home/<USER>/.Xresources
 	xrdb ~/.Xresources
-else grep -Fxq "URxvt*transparent: true" /home/gentoo/.Xresources
-	sed -i 's/URxvt\*transparent: true/URxvt\*transparent: false/g' /home/gentoo/.Xresources
+else grep -Fxq "URxvt*transparent: true" /home/<USER>/.Xresources
+	sed -i 's/URxvt\*transparent: true/URxvt\*transparent: false/g' /home/<USER>/.Xresources
 	xrdb ~/.Xresources
 fi
